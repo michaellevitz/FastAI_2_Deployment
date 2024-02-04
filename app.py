1e88554df8f9ec94372e79413f2e56fc04280513
Michael Levitz <michaellevitz@users.noreply.huggingface.co>
2024-02-04 02:44:43 +0000

    Upload app.py

--- app.py
+++ app.py
@@ -1,7 +1,20 @@
+from fastai.vision.all import *
 import gradio as gr
+def is_cat(x): return x[0].isupper()
 
-def greet(name):
-    return "Hello " + name + "!!"
+learn = load_learner('model.pkl')
 
-iface = gr.Interface(fn=greet, inputs="text", outputs="text")
-iface.launch()
+categories = ('Dog', 'Cat')
+
+def classify_image(img):
+    pred,idx,probs = learn.predict(img)
+    return dict(zip(categories, map(float,probs)))
+
+demo = gr.Interface(
+    fn=classify_image,
+    inputs=gr.Image(type="pil"),  # Image uploader
+    #outputs=gr.Textbox(label="Classification Result"),  # Textbox for the output
+    outputs=gr.Label(label="Classification Result"),  # Bar chart and label for the output
+    examples=['dog.jpg', 'cat.jpg', 'dunno.jpg']  # Example images
+)
+demo.launch(inline=False,share=True)
